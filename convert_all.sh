578370b83a63a6064ab26cc3d9d78445f18d5474
Kris Beevers <beevek@gmail.com>
2011-01-19 11:02:42 +0000

    adjustments to skip already converted files, and handle lowercase unzipped dirnames

--- convert_all.sh
+++ convert_all.sh
@@ -15,12 +15,27 @@ i=0
 
 for dnc in $(cat $DNCDIR/dncs.csv | grep -v BROWSE | awk -F, '{print $1 "/" $2}'); do
 	i=$(($i+1))
-	unzip -qq -d $TMPDIR $DNCDIR/$dnc.zip
-	chmod -R u+w $TMPDIR/DNC$dnc
 
 	src="$TMPDIR/DNC$dnc"
 	dest="$ENCDIR/$dnc.000"
+
 	echo "CONVERTING DNC $i OF $TOTAL ($src TO $dest)..."
+
+	if [ -f $dest ]; then
+		echo "$dest EXISTS, SKIPPING"
+		continue
+	fi
+
+	unzip -qq -d $TMPDIR $DNCDIR/$dnc.zip
+
+	if [ ! -e $TMPDIR/DNC$dnc -a -e $TMPDIR/dnc$dnc ]; then
+		# sometimes when you extract the zips they go in dncNN
+		# instead of DNCNN
+		mv $TMPDIR/dnc$(dirname $dnc) $TMPDIR/DNC$(dirname $dnc)
+	fi
+
+	chmod -R u+w $TMPDIR/DNC$dnc
+
 	mkdir -p $ENCDIR/$(dirname $dnc)
 	$DNC2ENC $src $dest
 
